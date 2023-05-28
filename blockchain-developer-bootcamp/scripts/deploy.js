65bdac72eced2f0e05b190e6bc07d92ee880e159
zubayertahmid <zubayertahmid2218@gmail.com>
2023-05-28 11:27:35 +0600

    Smoke Test Performed
    Made a token and performed the smoke test.

--- blockchain-developer-bootcamp/scripts/deploy.js
+++ blockchain-developer-bootcamp/scripts/deploy.js
@@ -7,25 +7,30 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  // const lockedAmount = hre.ethers.utils.parseEther("0.001");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // const Lock = await hre.ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  // await lock.deployed();
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // console.log(
+  //   `Lock with ${ethers.utils.formatEther(
+  //     lockedAmount
+  //   )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // );
+  //Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
+  //Deploy contract
+  const token = await Token.deploy() 
+  await token.deployed()
+  console.log(`Token Deployed to: ${token.address}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
