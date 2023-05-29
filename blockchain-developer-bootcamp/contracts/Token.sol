09854ca3eeba108d4b937f44ba4e58a6f8e3dbd5
zubayertahmid <zubayertahmid2218@gmail.com>
2023-05-29 21:10:39 +0600

    Some properties of ERC 20 Token were implemented

--- blockchain-developer-bootcamp/contracts/Token.sol
+++ blockchain-developer-bootcamp/contracts/Token.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 contract Token {
-	string public name = "My Token";
+	string public name = "Dapp University";
+	string public symbol = "DAPP";
+	uint256 public decimals = 18;
+	uint256 public totalSupply = 1000000*(10**decimals);
+
 
 }
--- blockchain-developer-bootcamp/test/Token.js
+++ blockchain-developer-bootcamp/test/Token.js
@@ -1,14 +1,45 @@
 const{ ethers } = require('hardhat');
 const { expect } = require('chai');
 describe("Token", ()=> {
-	it("has a name", async ()=>{
+	let token
+	beforeEach( async ()=>{
 		//Fetch token from blockchain
 		const Token = await ethers.getContractFactory("Token")
-		let token = await Token.deploy()
+		token = await Token.deploy()
+	})
+	it("has correct name", async ()=>{
+
 		//Read token name
 		const name = await token.name()
 		//Check that name is correct
-		expect(name).to.equal("My Token")
+		expect(await token.name()).to.equal("Dapp University")
+
+	})
+
+	it("has correct symbol", async ()=>{
+		
+		//Read token name
+		const symbol = await token.symbol()
+		//Check that name is correct
+		expect(await token.symbol()).to.equal("DAPP")
+
+	})
+
+	it("has correct decimals", async ()=>{
+		
+		//Read token name
+		const decimals = await token.decimals()
+		//Check that name is correct
+		expect(await token.decimals()).to.equal('18')
+
+	})
+
+	it("has correct total supply", async ()=>{
+		
+		//Read token name
+		const decimals = await token.totalSupply()
+		//Check that name is correct
+		expect(await token.totalSupply()).to.equal('1000000000000000000000000')
 
 	})
 })
