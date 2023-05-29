f5b535cfcb19121f1258489c904824045671c7e8
zubayertahmid <zubayertahmid2218@gmail.com>
2023-05-29 21:50:31 +0600

    Constructors are made
    The constructors for the properties of the tokens were made.

--- blockchain-developer-bootcamp/contracts/Token.sol
+++ blockchain-developer-bootcamp/contracts/Token.sol
@@ -1,10 +1,14 @@
 pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 contract Token {
-	string public name = "Dapp University";
-	string public symbol = "DAPP";
+	string public name;
+	string public symbol;
 	uint256 public decimals = 18;
-	uint256 public totalSupply = 1000000*(10**decimals);
+	uint256 public totalSupply;
 
-
+	constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
+		name = _name;
+		symbol = _symbol;
+		totalSupply = _totalSupply*(10**decimals);
+	}
 }
--- blockchain-developer-bootcamp/test/Token.js
+++ blockchain-developer-bootcamp/test/Token.js
@@ -1,11 +1,14 @@
 const{ ethers } = require('hardhat');
 const { expect } = require('chai');
+const tokens = (n) => {
+	return ethers.utils.parseUnits(n.toString(), 'ether')
+}
 describe("Token", ()=> {
 	let token
 	beforeEach( async ()=>{
 		//Fetch token from blockchain
 		const Token = await ethers.getContractFactory("Token")
-		token = await Token.deploy()
+		token = await Token.deploy("Dapp University", "DAPP", '1000000')
 	})
 	it("has correct name", async ()=>{
 
